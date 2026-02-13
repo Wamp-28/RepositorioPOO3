6bbdb90c624eb32dfa7a43f8e5a37f35ff99dc37
AlexM <wmatallana28@gmail.com>
2026-02-13 17:16:41 -0500

    Comandos git

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,12 @@ public class Main {
         System.out.println("NOMBRES - JAVA");
 
 
+        System.out.println("1. COMANDOS DE GIT VISTOS");
+        System.out.println("1. git add .");
+        System.out.println("2. GIT COMMIT -M");
+        System.out.println("3. GIT PUSH");
+        System.out.println("4. GIT SWITCH");
+
 
 
 
